69c765c3e73667931a27291eca8fbe2d9d10eaf7
kdh <kdhmm0325@naver.com>
2025-02-14 17:18:53 +0900

    server.js update

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-var http = require("http");
-var fs = require("fs");
+var fs = require('fs');
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("demofile1.html", function (err, data) {
-      res.write(data);
-      fs.readFile("demofile1.html", function (err, data) {
-        res.write(data);
-        res.end();
-      });
-    });
-  })
-  .listen(3000);
+var rs = fs.createReadStream('./demofile1.html');
 
-  var http = require("http");
-var fs = require("fs/promises");
-
-http.createServer(async function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    const data = await fs.readFile("demofile1.html");
-    res.write(data);
-    const data2 = await fs.readFile("demofile1.html");
-    res.write(data2);
-    res.end();
-  })
-  .listen(3000);
+rs.on('open', function () {
+  console.log('The file is open');
+});
